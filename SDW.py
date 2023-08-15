14b71ef70ac02e700921ce8711d2c2de94924815
AuthorSultra <128295202+AuthorSultra@users.noreply.github.com>
2023-08-15 20:35:34 +0800

    Update SDW.py

--- SDW.py
+++ SDW.py
@@ -1,11 +1,10 @@
-
-cd /content
+%cd /content
 !pip install pycloudflared
 !pip install insightface==0.7.3
-pip install onnx==1.14.0
-pip install onnxruntime==1.15.0
-pip install opencv-python==4.7.0.72
-pip install tqdm
+!pip install onnx==1.14.0
+!pip install onnxruntime==1.15.0
+!pip install opencv-python==4.7.0.72
+!pip install tqdm
 sde = "stable"
 sdq= "-diffusion-webui"
 sdw = sde+sdq
@@ -21,5 +20,4 @@ tunk = "-tunnels"
 rops = "-roop"
 swaprop = sd+rops
 tunell = sd+tunk
-%cd /content
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
+!git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
