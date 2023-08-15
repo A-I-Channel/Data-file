5ea025a5668c331be49c9e7f106caef2316c3eda
AuthorSultra <128295202+AuthorSultra@users.noreply.github.com>
2023-08-15 20:30:13 +0800

    Update SDW.py

--- SDW.py
+++ SDW.py
@@ -1,7 +1,7 @@
 
 cd /content
 !pip install pycloudflared
-pip install insightface==0.7.3
+!pip install insightface==0.7.3
 pip install onnx==1.14.0
 pip install onnxruntime==1.15.0
 pip install opencv-python==4.7.0.72
