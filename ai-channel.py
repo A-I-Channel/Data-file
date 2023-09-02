f67b53bd5492ad3494037b6f8031cfe12a654809
AuthorSultra <128295202+AuthorSultra@users.noreply.github.com>
2023-09-02 20:21:36 +0800

    Update ai-channel.py

--- ai-channel.py
+++ ai-channel.py
@@ -101,3 +101,4 @@ https://github.com/A-I-Channel/sd-openpose-editor-clone
 -b v2.5 https://github.com/camenduru/ui /content/stable-diffusion-webui
 https://github.com/A-I-Channel/ui-clone /content/stable-diffusion-webui
 https://github.com/A-I-Channel/AI_Channel_Theme
+https://github.com/deforum-art/sd-webui-deforum
